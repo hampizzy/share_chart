571819c9e9bf404a8a6691571c9b4ea8676ef3a5
Carl McMillan <hampizzy@gmail.com>
2011-04-24 20:35:03 +1200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShareChart::Application.config.secret_token = '61193bbbb3416e96daebcc5627d903cc2a46a3e18a583045f3a98bc5b6d163666b421101340e0793917061d8c43e50775d6732897a2cd965d411b1c2bd15bba3'
