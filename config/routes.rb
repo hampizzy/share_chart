4909b68417a2cfa189c105be425196c480910710
Carl McMillan <hampizzy@gmail.com>
2011-04-25 01:22:58 +1200

    Added pages and styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 ShareChart::Application.routes.draw do
+  root              :to => 'pages#home'
+  match '/chart',   :to => 'pages#chart'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
---
+++ test/functional/pages_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class PagesControllerTest < ActionController::TestCase
+  test "should get home" do
+    get :home
+    assert_response :success
+  end
+
+  test "should get chart" do
+    get :chart
+    assert_response :success
+  end
+
+end
